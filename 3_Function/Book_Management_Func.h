34ceb05c6583caa8dfbfa3cb32f895f1423d208d
kittipichcha <104352517+kittipichcha@users.noreply.github.com>
2025-04-26 21:19:32 +0700

    Solve the merge problem

--- 3_Function/Book_Management_Func.h
+++ 3_Function/Book_Management_Func.h
@@ -10,26 +10,10 @@
 
 int filterCategory(char listName[][max_char], int number_list);
 int filterUsable(char filter[], int number_filter);
-<<<<<<< HEAD
-void addBook();
-void editBook();
-void deleteBook(booksNode* head, booksNode* prev, booksNode* node);
-void saveCSV();
-booksNode* bookParam(int cateIndex, int year, char title[], char author[], int quantity);
-int* extractID(char id[15]);
-void showAllBooks();
-void showBookList(booksNode* temp);
-void showBookData(booksNode* temp);
-int checkDataEachList(booksListInfo linkList, char userInput[]);
-int isNotInteger(const char* str);
-
-#endif
-=======
 int filterCategory();
 void showAllBooks();
 void extractID();
 void addBookParam();
-void deleteBook();
 void showBookList();
 void showBookData();
 int checkDataEachList();
@@ -38,4 +22,6 @@ int searchBook();
 void addBook();
 void editBook();
 void saveCSV();
->>>>>>> d80f37263999ff9f76010afcf268c9ab8d36e9dd
+void deleteBook();
+void delete();
+#endif
