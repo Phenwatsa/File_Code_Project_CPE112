cf9be8fefa1e694b1fb5ef904570defb37242dca
lephirada <phirada.lekp@mail.kmutt.ac.th>
2025-05-01 20:54:40 +0700

    แยกกันเสร็จละ

--- 3_Function/Other_Func.c
+++ 3_Function/Other_Func.c
@@ -63,3 +63,79 @@ int Check_User_ID(const char* str){
     return 1;
 }
 
+
+
+
+
+
+//Display top 5 borrowed books
+
+int loadBooksFromCSV(const char *fileName, Book books[])
+{
+    FILE *fp = fopen(fileName, "r");
+    if (!fp)
+    {
+        printf("Cannot open file"); ////////ui
+        return 0;
+    }
+    
+    char line[MAX_LINE];
+    int count = 0;
+    fgets(line, sizeof(line), fp );
+    while (fgets(line, sizeof(line), fp ) != NULL && count < MAX_BOOKS)
+    {
+        char *comma_firstposi = strchr(line, ',');
+        if (comma_firstposi == NULL) continue;
+
+        int len_id = comma_firstposi - line;
+        if (len_id >= MAX_ID)
+        {
+           len_id = MAX_ID-1;
+        }
+        
+        strncpy(books[count].ID, line,len_id);
+        books[count].ID[len_id] = '\0';
+        
+        sscanf(comma_firstposi + 1, "%[^,],%*[^,],%*[^,],%*d,%*d,%*[^,],%d",books[count].TITLE,&books[count].BORROWED);
+        count++;
+        
+      
+        
+    }
+
+    fclose(fp);
+    return count;
+    
+}
+
+
+int compareByBorrowed(const void *a, const void *b) {
+    Book *bookA = (Book *)a;
+    Book *bookB = (Book *)b;
+    return bookB->BORROWED - bookA->BORROWED; 
+}
+
+void displayTop5BorrowedBooks(Book books[], int count)
+{
+
+    qsort(books,count,sizeof(Book),compareByBorrowed);
+    printf("Top 5 Most Borrowed Books\n");
+    printf("%-20s %-90s %-20s\n", "Borrowed", "Title", "Book ID"); ////////ีรรรuiiiii
+
+    int top5;
+    if (count < 5)
+    {
+        top5 = count;
+    }else
+    {
+        top5 = 5;
+    }
+    
+    for (int i = 0; i < top5; i++)
+    {
+        printf("%-20d %-90s %-20s\n",books[i].BORROWED,books[i].TITLE,books[i].ID); //////ีรรรร  uiiiii
+
+    }
+    
+
+}
--- 3_Function/Other_Func.h
+++ 3_Function/Other_Func.h
@@ -1,6 +1,14 @@
 #ifndef OTHER_FUNC_H
 #define OTHER_FUNC_H
 
+#define MAX_BOOKS 1000
+#define MAX_LINE 256
+#define MAX_ID 15  
+#define MAX_TITLE 200    
+#define MAX_AUTHER 100    
+#define MAX_CATEGORY 100 
+#define MAX_STATUS 20
+
 // UI design format
 void Line();
 void Line2();
@@ -9,6 +17,22 @@ void Line4(int num);
 void ClearScreen();
 void Delay();
 void Exit();
+
+
+
+typedef struct {
+    char ID[MAX_ID];
+    char TITLE[MAX_TITLE];
+    char AUTHOR[MAX_AUTHER];
+    char CATEGORY[MAX_CATEGORY];
+    int YEAR;
+    int QUANTITY;
+    char STATUS[MAX_STATUS];
+    int BORROWED;
+} Book;
+
+int loadBooksFromCSV(const char *fileName, Book books[]);
+void displayTop5BorrowedBooks(Book books[], int count);
 
 int Check_Num(const char* str);
 int Check_User_ID(const char* str);
